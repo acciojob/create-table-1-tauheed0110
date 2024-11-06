b74d1314c2e10ab0363e12e63824f075b7c49f1d
tauheed0110 <tauheed0110@gmail.com>
2024-11-06 09:31:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
+const table = document.getElementById("sampleTable");
+
 function insert_Row() {
-    //Write your code here
-  
-  
+    table.innerHTML = `
+    <tr>
+        <td>New cell1</td>
+        <td>New cell2</td>
+    </tr>
+  ` + table.innerHTML;
 }
